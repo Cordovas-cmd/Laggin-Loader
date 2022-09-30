e9472f3a4e6d3068a97520e1b373bfcac36a76c5
samuel cordova <cordovas1337@gmail.com>
2022-09-30 03:58:03 -0500

    added a function and set interval for loading, clear it at 100 was able to console log moving on to html

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ const bg = document.querySelector('.bg');
 // start load off at 0 and we need to increment it every so often to both show load progress and reveal the background image.
 let load = 0;
 
+
+let int = setInterval(bgBlur, 30);
+
+function bgBlur() {
+    load++
+
+    if(load > 99) {
+        clearInterval(int);
+    }
+
+      console.log(load)
+}
