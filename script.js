c5b29a606b02d77b81ba7556c06639d78c00145e
samuel cordova <cordovas1337@gmail.com>
2022-09-30 03:53:54 -0500

    finished work on the css for now starter js in place

---
+++ script.js
@@ -0,0 +1,5 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+// start load off at 0 and we need to increment it every so often to both show load progress and reveal the background image.
+let load = 0;
+
