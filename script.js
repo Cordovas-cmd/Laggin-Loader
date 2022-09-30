dd6824acdc021c9192a8188de44a9014d5e33946
samuel cordova <cordovas1337@gmail.com>
2022-09-30 07:55:30 -0500

    added an event listener for a mouse entering the button zone that will rotate the bg, now need to reset transform

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const loadText = document.querySelector('.loading-text');
-const bg = document.querySelector('.bg');
+let bg = document.querySelector('.bg');
 // start load off at 0 and we need to increment it every so often to both show load progress and reveal the background image.
 let load = 0;
 
@@ -15,21 +15,44 @@ function bgBlur() {
        btn.innerHTML="Enter"
 
        document.body.appendChild(btn);
-    //    window.location.replace("http://www.w3schools.com");
 
-        clearInterval(int);
+       header = document.createElement('h1');
+       header.classList.add('header');
+       header.innerHTML="Laugh and the world laughs with you..."
+       document.body.appendChild(header);
+       //    window.location.replace("http://www.w3schools.com");
+       
+       clearInterval(int);
+       btn.addEventListener('mouseenter', function () { 
+        console.log(bg)
+        flipBg(bg)
+        console.log("hello!");
+        setTimeout(() => {
+            bg.style.transform = "";
+          }, 500);   
+    })
     }
-
+    
     loadText.innerText = `${load}%`  
     // need to map 0 to 100 to 1 to 0
     loadText.style.opacity = scale(load, 0, 100, 1.6, 0)
     bg.style.filter= `blur(${scale(load, 0, 100, 30, 0)}px`
-      console.log(load)
+    //   console.log(load)
 }
+let button= document.querySelector('button')
 
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
-  }
+}
+
+function flipBg(element) {
+    element.style.transform = 'scale(-1)';
+    console.log("hello world")
+
+}
+
+// button.addEventListener('mouseover', () => { flipBg()})
+// btn.addEventListener('mouseover', () => { flipBg()})
